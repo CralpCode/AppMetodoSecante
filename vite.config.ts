860a66e6a3e906b3c31400cd220f3a1e717727ab
CarlosL <1.carlosraullopez@gmail.com>
2026-06-16 11:19:24 -0600

    Configurar despliegue de GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/AppMetodoSecante/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
@@ -12,9 +13,7 @@ export default defineConfig(() => {
       },
     },
     server: {
-      // Disable HMR when DISABLE_HMR is set to true.
       hmr: process.env.DISABLE_HMR !== 'true',
-      // Disable file watching when DISABLE_HMR is true to save CPU.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
